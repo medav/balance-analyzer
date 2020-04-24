4dcfba511014d3a9eff6ef3e9bda17cd0d5818a2
Michael Davies <michaelstoby@gmail.com>
2020-04-24 15:14:09 -0500

    Update softbrain.h with needed features to support code samples

--- test-programs/include/softbrain.h
+++ test-programs/include/softbrain.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+typedef unsigned long long int bound_t;
+
 void SB_CONFIG() { }
 void SB_WAIT() { }
 
@@ -27,3 +29,17 @@ void SB_PORT_MEM_STREAM(
 void SB_DISCARD(
     int outport,
     uint16_t nelems) { }
+
+void SB_MEM_SCRATCH_STREAM(
+    const void * mem_addr,
+    uint16_t stride,
+    uint16_t access_size,
+    uint16_t nstrides,
+    int addr) { }
+
+#define SB_SCRATCH_PORT_STREAM(addr, stride, access_size, num_iters, port) \
+    SB_MEM_PORT_STREAM(addr, stride, access_size, num_iters, port)
+
+#define SBBAR_SCRATCH_WRITE 0
+#define SBBAR_SCRATCH_READ 1
+void SB_BARRIER(int type) { }
